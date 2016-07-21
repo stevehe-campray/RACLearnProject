778d7ba3592a68146d8b71b1e1eff909d190daba
stevehe-campray <hejingjin@192.168.0.125>
2016-07-21 14:48:18 +0800

    update submit
    update  submit

--- LearnRacProject/LearnRacProject/loginViewController.m
+++ LearnRacProject/LearnRacProject/loginViewController.m
@@ -17,7 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
-    self.view.backgroundColor = [UIColor blueColor];
+    self.view.backgroundColor = [UIColor whiteColor];
+    [self SetUpsigle];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -25,14 +26,33 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(void)SetUpsigle{
+    
+    RACSignal *usernamesignal = [self.nameLabel.rac_textSignal map:^id(NSString *value) {
+        NSLog(@"%@",value);
+        NSInteger length = value.length;
+        if (length > 1) {
+            return @(YES);
+        }
+            return @(NO);
+    }];
+    
+    RAC(self.submitButton,enabled) = [RACSignal combineLatest:@[usernamesignal] reduce:^id(NSNumber *isUsernameCorrect){
+        if (isUsernameCorrect.boolValue) {
+            [self.submitButton setTitle:@"111" forState:UIControlStateNormal];
+        }else{
+            [self.submitButton setTitle:@"提交" forState:UIControlStateNormal];
+        }
+        return @(isUsernameCorrect.boolValue);
+    }];
+    
+    [self.submitButton addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
+    
+    
 }
-*/
+
+-(void)buttonPressed:(UIButton *)submitButton{
+    NSLog(@"1111");
+}
 
 @end
